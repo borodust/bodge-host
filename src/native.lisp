adad5d431a0265cceb07d636ac9b928cb12cf29e
Pavel Korolev <dev@borodust.org>
2019-01-05 21:30:21 +0300

    Copy screen region unsafely for now

--- src/native.lisp
+++ src/native.lisp
@@ -1,5 +1,6 @@
 (cl:in-package :bodge-host)
 
 
-(defun read-screen-region (x y width height data-ptr)
-  (bodge-host.native:read-screen-region x y width height data-ptr))
+(defun read-screen-region (x y width height image-array)
+  (bodge-util:with-simple-array-pointer (ptr image-array)
+    (bodge-host.native:read-screen-region x y width height ptr)))
